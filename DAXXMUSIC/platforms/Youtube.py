aba9136b07f6286a75273f3896f0084860647250
Un-bots <161623523+Un-bots@users.noreply.github.com>
2025-12-29 13:04:43 +0530

    Update Youtube.py

--- DAXXMUSIC/platforms/Youtube.py
+++ DAXXMUSIC/platforms/Youtube.py
@@ -213,25 +213,33 @@ class YouTubeAPI:
         return result
 
     async def track(self, link: str, videoid: Union[bool, str] = None):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            title = result["title"]
-            duration_min = result["duration"]
-            vidid = result["id"]
-            yturl = result["link"]
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        track_details = {
-            "title": title,
-            "link": yturl,
-            "vidid": vidid,
-            "duration_min": duration_min,
-            "thumb": thumbnail,
-        }
-        return track_details, vidid
+    if videoid:
+        link = self.base + link
+    if "&" in link:
+        link = link.split("&")[0]
+
+    ydl_opts = {
+        "quiet": True,
+        "nocheckcertificate": True,
+        "cookiefile": cookie_txt_file(),
+        "default_search": "ytsearch1",
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(link, download=False)
+
+    if "entries" in info:
+        info = info["entries"][0]
+
+    track_details = {
+        "title": info.get("title"),
+        "link": info.get("webpage_url"),
+        "vidid": info.get("id"),
+        "duration_min": info.get("duration"),
+        "thumb": info.get("thumbnail"),
+    }
+
+    return track_details, track_details["vidid"]
 
     async def formats(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
